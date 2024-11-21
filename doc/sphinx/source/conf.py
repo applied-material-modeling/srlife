8f0f0e908c1659f90414af3fea1240ccd42c19ca
Mark Messner <mark.messner@gmail.com>
2024-11-21 10:44:46 -0600

    Increment version (#3)
    * Update version
    
    * Updated readme

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2024, Argonne National Laboratory'
 author = 'Argonne National Laboratory'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.0.2'
 
 
 # -- General configuration ---------------------------------------------------
