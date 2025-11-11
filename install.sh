f6b37560d6cffbfc0e3daffa8f80c2a7b9e807c0
sagarbhatt0904 <sagar.bhatt0904@gmail.com>
2025-11-11 15:32:17 -0600

    included solid Mech module build and  ensuring conda shell fucntions are available

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ BUILD_MOOSE=false
 if [[ "$1" == "--with-moose" ]]; then
     BUILD_MOOSE=true
     echo "Installing srlife with MOOSE..."
-    # Initialize MOOSE submodule without recursion (avoids large media files)
+    # Initialize MOOSE submodule
     git submodule update --init moose
     cd moose
     git checkout master
@@ -22,7 +22,18 @@ MOOSE_JOBS=32
 
 # Create conda environment
 echo "Creating conda environment..."
-conda create -y -n moose moose-dev=2025.09.18=mpich
+# ensure conda shell functions are available in this script
+eval "$(conda shell.bash hook)"
+
+# Create the environment only if it doesn't already exist
+if conda env list | awk '!/^#/ && NF>0 {print $1}' | grep -Fxq "$ENV_NAME"; then
+    echo "Conda environment '$ENV_NAME' already exists; skipping creation."
+else
+    conda create -n "$ENV_NAME" moose-dev=2025.09.18=mpich
+    #TODO:install seacas --- maybe add this to requirements.txt later
+    conda install moose-seacas
+fi
+
 
 # Activate environment
 conda activate $ENV_NAME
@@ -31,6 +42,9 @@ conda activate $ENV_NAME
 echo "Installing Python dependencies..."
 pip3 install --user wheel
 pip3 install -r requirements.txt
+
+
+
 
 # Build MOOSE (if specified)
 if [[ "$BUILD_MOOSE" == "true" ]]; then
@@ -42,15 +56,21 @@ if [[ "$BUILD_MOOSE" == "true" ]]; then
     echo "Installing Thermohydraylics module..."
     cd $SRLIFE_DIR/moose/modules/thermal_hydraulics/
     make -j$MOOSE_JOBS
+    
+    echo "Installing Solid Mechanics module..."
+    cd $SRLIFE_DIR/moose/modules/solid_mechanics/
+    make -j$MOOSE_JOBS
 fi
 
 echo "Installation complete!"
 echo ""
 if [[ "$BUILD_MOOSE" == "true" ]]; then
-    echo "To use:"
+    echo "To use add the following to your rc file:"
     echo "  conda activate $ENV_NAME"
     echo "  export PYTHONPATH=$SRLIFE_DIR/"
     echo "  export MOOSE_THM=$SRLIFE_DIR/moose/modules/thermal_hydraulics/thermal_hydraulics-opt"
+    echo "  export MOOSE_SM=$SRLIFE_DIR/moose/modules/thermal_hydraulics/solid_mechanics-opt"
+    echo "  export MOOSE_MPI=$(which mpirun)"
 else
     echo "To use:"
     echo "  conda activate $ENV_NAME"
